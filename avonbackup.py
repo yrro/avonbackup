699a94bf455fd665a09ffd354bbea6b006807b7a
Sam Morris <sam@robots.org.uk>
2013-01-01 13:04:43 +0000

    Add locking

--- avonbackup.py
+++ avonbackup.py
@@ -1,18 +1,29 @@
 def main ():
 	args = parse ()
 
+	if args.lockfile:
+		lock (args.lockfile)
 	unshare ()
 	bind_mount ('/', args.temp_mount)
 	obnam (args.repository, args.temp_mount)
 
 	if args.sleep:
+		print 'Backup complete!'
 		import time
 		while True:
 			time.sleep (86400)
 
+def lock (lockfile):
+	import os
+	l = os.open (lockfile, os.O_WRONLY | os.O_CREAT)
+	os.fchmod (l, 0666)
+	import fcntl
+	fcntl.lockf (l, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
 def parse ():
 	import argparse
 	parser = argparse.ArgumentParser ()
+	parser.add_argument ('--lockfile', help='Prevent concurrent instances from running by locking this file')
 	parser.add_argument ('--temp-mount', help='Path to temporary mount point used during backup', default='/mnt')
 	parser.add_argument ('--sleep', help='Sleep when done', action='store_true')
 	parser.add_argument ('repository', help='Path to the target obnam repository')
@@ -33,12 +44,10 @@ def bind_mount (source, target):
 	mount (source, target, None, MS_MGC_VAL | MS_BIND, None)
 
 def obnam (repository, source):
+	obnam_args = (['obnam', '-r', repository])
 	import subprocess
-	subprocess.check_call (['obnam',
-		'-r', repository,
-		'--one-file-system',
-		'backup',
-		source])
+	subprocess.check_call (obnam_args + ['force-lock', source])
+	subprocess.check_call (obnam_args + ['backup', source])
 
 if __name__ == '__main__':
 	main ()
